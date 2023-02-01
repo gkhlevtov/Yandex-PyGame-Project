be4f94cdaeb90f56f00724e9e0ca394471b1b384
gkhlevtov <ghkhlevtov@mail.ru>
2023-02-02 01:13:45 +0200

    add new functions and class

--- globals.py
+++ globals.py
@@ -1,11 +1,25 @@
+import csv
 import os
 import sys
 
+import matplotlib.pyplot
 import pygame
 import pygame.mixer as mixer
 
 pygame.init()
 mixer.init()
+
+
+class RunWindow:
+    """Класс запуска нового окна."""
+
+    def __init__(self, window):
+        self.window = window
+
+    def run(self):
+        self.window.main()
+        pygame.quit()
+        sys.exit()
 
 
 class Button(pygame.sprite.Sprite):
@@ -94,17 +108,48 @@ def load_image(filename, color_key=None):
     return image
 
 
-class RunWindow:
-    """Класс запуска нового окна."""
+def get_max_score_row():
+    """Функция получения строки с лучшим счётом."""
+    rows = []
+    with open('data.csv', encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for index, row in enumerate(reader):
+            rows.append(row)
+        rows = rows[1:]
 
-    def __init__(self, window):
-        self.window = window
-        print(window)
+    return sorted(rows, key=lambda x: -int(x[1]))[0]
 
-    def run(self):
-        self.window.main()
-        pygame.quit()
-        sys.exit()
+
+def make_graph(table_name='data.csv', file_name='graph.png'):
+    """Функция создания графика очков."""
+    fig, ax = matplotlib.pyplot.subplots(figsize=(40, 30))
+
+    rows = []
+    with open(table_name, encoding="utf8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for index, row in enumerate(reader):
+            rows.append(row)
+
+    rows = rows[1:]
+    if len(rows) > 15:
+        rows = rows[-15:]
+
+    x = [f'{row[0]} {row[3]}' for row in rows]
+    y = [int(row[1]) for row in rows]
+
+    ax.bar(x, y, color='orange')
+    ax.set_facecolor('gray')
+
+    ax.set_xlabel('Номер игры, дата', color='white', fontsize=70)
+    ax.set_ylabel('Очки', color='white', fontsize=70)
+
+    matplotlib.pyplot.xticks(rotation=30, ha='right', color='white', fontsize=40)
+    matplotlib.pyplot.yticks(color='white', fontsize=50)
+    matplotlib.pyplot.grid(color='black', linestyle=':', linewidth=5)
+
+    fig.set_facecolor('forestgreen')
+
+    fig.savefig(f'images/{file_name}', transparent=True)
 
 
 click_sound = mixer.Sound('sounds/click.mp3')
@@ -117,6 +162,9 @@ w_percent = display_width // 100
 h_percent = display_height // 100
 
 card_size = 225 * display_width // 1920, 315 * display_height // 1080
+
+use_custom_cursor = True
+fps = 60
 
 # Полная колода карт
 full_deck = (
